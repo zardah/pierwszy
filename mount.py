b2985ae298e1e29e1ce99b00289096a7eab6b126
Zardah <eurynom83@wp.pl>
2017-04-04 15:24:37 +0200

    Dodane instrukcje warunkową i while..break wewnatrz if

--- mount.py
+++ mount.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import subprocess
+# from fabric.api import *  (nie widzi fabric)
+
+
+chec = input("Czy chcesz zsynchronizować dane? Wpisz 'tak/nie' ")
+
+while True:
+    if chec == "nie":
+        print("Dane nie zostana wyswietone")
+        break
+    elif chec == "tak":
+        print("Dane zostana zsynchronizowane")
+        subprocess.call('/sbin/blkid -o udev')
+        break
+    else:
+        print("Wpisales cos innego, niz tak/nie. Wpisz ponownie.")
+        chec = input("Wpisz teraz: ")
+
+
+
+
+
+
 
